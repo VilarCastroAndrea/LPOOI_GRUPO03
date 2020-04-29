2a6470dc78a15bb2c400f87e6d7e453f4126c9ac
RENE <RENE@MRI-BANGHO-I7>
2020-04-29 20:54:11 -0300

    conexion  A bd

--- LPOOI_GRUPO03/ClasesBase/Properties/Settings.Designer.cs
+++ LPOOI_GRUPO03/ClasesBase/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace ClasesBase.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\andii\\source\\repos\\L" +
-            "POOI_GRUPO03\\AgenciaDB.mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\RENE\\source\\repos\\LP" +
+            "OOI_GRUPO03\\AgenciaDB.mdf;Integrated Security=True;Connect Timeout=30")]
         public string AgenciaDBConnectionString {
             get {
                 return ((string)(this["AgenciaDBConnectionString"]));
