afc4130fc9dde1f2bc3572aa53cf9fa746ce4eb2
josue487 <josue487@gmail.com>
2020-05-06 17:54:15 -0300

    creacion del formulario vacio y coneccion con bd

---
+++ LPOOI_GRUPO03/Vistas/FrmRegistrarVenta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ClasesBase;
+
+namespace Vistas
+{
+    public partial class FrmRegistrarVenta : Form
+    {
+        public FrmRegistrarVenta()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbClientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmRegistrarVenta_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
