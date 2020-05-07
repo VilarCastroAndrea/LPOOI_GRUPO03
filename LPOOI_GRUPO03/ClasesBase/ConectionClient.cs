bff9e0ff56b10a0f250ff64160ac40bc38126fb6
RENE <RENE@MRI-BANGHO-I7>
2020-05-06 22:36:08 -0300

    Insertar Cliente y user en clases base

--- LPOOI_GRUPO03/ClasesBase/ConectionClient.cs
+++ LPOOI_GRUPO03/ClasesBase/ConectionClient.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ClasesBase
 {
     class ConectionClient
     {
+        public static void InsertClient(Cliente client)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Cliente(CLI_DNI,CLI_Nombre,CLI_Apellido,CLI_Direccion,CLI_Telefono) values(@dni,@name,@apll,@dir,@tel)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@dni", client.Cli_DNI);
+            cmd.Parameters.AddWithValue("@name", client.Cli_Nombre);
+            cmd.Parameters.AddWithValue("@apll", client.Cli_Apellido);
+            cmd.Parameters.AddWithValue("@dir", client.Cli_Direccion);
+            cmd.Parameters.AddWithValue("@tel", client.Cli_Telefono);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+
+        }
     }
 }
--- LPOOI_GRUPO03/ClasesBase/ConectionUser.cs
+++ LPOOI_GRUPO03/ClasesBase/ConectionUser.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace ClasesBase
 {
     class ConectionUser
     {
+        public static void InsertUser( Usuario user)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Usuario(USU_NombreUsuario,USU_Password,USU_ApellidoNombre,ROL_Codigo) values(@name,@pswd,@ayn,@rol)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@name",user.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@pswd", user.Usu_Contraseña);
+            cmd.Parameters.AddWithValue("@ayn", user.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@rol", user.Rol_Codigo);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+
+        }
     }
 }
