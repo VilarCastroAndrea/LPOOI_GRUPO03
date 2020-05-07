330cdef3e332b35ff3e72e28149b6040baf20a64
luis herrera <luisgherrera44@gmail.com>
2020-05-06 22:39:21 -0300

    termine todos los metodos de usuario

--- LPOOI_GRUPO03/ClasesBase/TrabajoUsuario.cs
+++ LPOOI_GRUPO03/ClasesBase/TrabajoUsuario.cs
@@ -24,5 +24,56 @@ namespace ClasesBase
             da.Fill(dt);
             return dt;
         }
+
+        public static void AgregarUsuario(Usuario usuario)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Usuario(USU_NombreUsuario,USU_Password,USU_ApellidoNombre,ROL_Codigo) values (@nomUsu,@pass,@ayp,@rol)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@nomUsu", usuario.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@pass", usuario.Usu_Contraseña);
+            cmd.Parameters.AddWithValue("@ayn", usuario.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@rol", usuario.Rol_Codigo);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static DataTable buscarUsuario(String nombreUsuario)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Usuario";
+            cmd.CommandText += " WHERE USU_NombreUsuario LIKE @nombreUsuario";
+
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@nombreUsuario", "%" + nombreUsuario + "%");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            return dt;
+        }
+
+        public static int eliminarUsuario(int idUsu)
+        {
+            int retorno = 0;
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+
+            SqlCommand cmd = new SqlCommand(string.Format("Delete From Usuario Where id={ 0 }",idUsu), cnn);
+            retorno = cmd.ExecuteNonQuery();
+            cnn.Close();
+
+            return retorno;
+        }
     }
 }
