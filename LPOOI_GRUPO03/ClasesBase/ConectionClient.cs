7d4495d26737803c9ca3008179997fa73e297565
RENE <RENE@MRI-BANGHO-I7>
2020-05-07 10:52:06 -0300

    Lista y busqueda en COnexionClient

--- LPOOI_GRUPO03/ClasesBase/ConectionClient.cs
+++ LPOOI_GRUPO03/ClasesBase/ConectionClient.cs
@@ -30,5 +30,62 @@ namespace ClasesBase
 
 
         }
+        public static DataTable ListClient()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT";
+            cmd.CommandText += " CLI_DNI as 'Dni', ";
+            cmd.CommandText += " CLI_Nombre as 'Nombre', ";
+            cmd.CommandText += " CLI_Apellido as 'Apellido', ";
+            cmd.CommandText += " CLI_Direccion as 'Direccion', ";
+            cmd.CommandText += " CLI_Telefono as 'Telefono', ";
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            //ejecuta la consulta
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //llena los datos de la consulta en el datatable
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            return dt;
+
+
+        }
+        public static DataTable SearchClient( string apellido)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT";
+            cmd.CommandText += " CLI_DNI as 'Dni', ";
+            cmd.CommandText += " CLI_Nombre as 'Nombre', ";
+            cmd.CommandText += " CLI_Apellido as 'Apellido', ";
+            cmd.CommandText += " CLI_Direccion as 'Direccion', ";
+            cmd.CommandText += " CLI_Telefono as 'Telefono', ";
+            cmd.CommandText += " FROM Cliente as C";
+
+            cmd.CommandText += " WHERE";
+            cmd.CommandText += " CLI_Apellido LIKE @apellido";
+            
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@apellido" ,"%"+apellido+"%");
+
+            //ejecuta la consulta
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //llena los datos de la consulta en el datatable
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            return dt;
+
+
+        }
     }
 }
