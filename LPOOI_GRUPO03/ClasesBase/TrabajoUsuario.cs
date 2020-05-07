a311da805d4dcd159ce0dae45ea651899357d83a
luis herrera <luisgherrera44@gmail.com>
2020-05-06 22:07:03 -0300

    Metodo listarUsuario

---
+++ LPOOI_GRUPO03/ClasesBase/TrabajoUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    class TrabajoUsuario
+    {
+
+        public static DataTable listarUsuario()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Usuario";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
