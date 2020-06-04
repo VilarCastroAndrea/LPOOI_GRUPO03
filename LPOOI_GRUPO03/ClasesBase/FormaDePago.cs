f44ad2c4d2e7f63faf53d32591e72f3ff4db6ea6
luis herrera <luisgherrera44@gmail.com>
2020-06-04 17:00:42 -0300

    clases base de forma de pago

---
+++ LPOOI_GRUPO03/ClasesBase/FormaDePago.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesBase
+{
+    class FormaDePago
+
+    {
+       private int fp_ID;
+        private String fp_Descripcion;
+
+        public FormaDePago(int fp_ID, string fp_Descripcion)
+        {
+            this.fp_ID = fp_ID;
+            this.fp_Descripcion = fp_Descripcion;
+        }
+
+        public FormaDePago()
+        {
+        }
+
+        public int Fp_ID { get => fp_ID; set => fp_ID = value; }
+        public string Fp_Descripcion { get => fp_Descripcion; set => fp_Descripcion = value; }
+
+    }
+}
---
+++ LPOOI_GRUPO03/ClasesBase/TrabajarFormaPago.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    class TrabajarFormaPago
+    {
+        public static void InsertarFormaPago(FormaDePago formaPago)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "insertarFormaPago";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@descripcion", formaPago.Fp_Descripcion);
+            
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static DataTable ListaFormaDePago()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.AgenciaDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "ListarFormaPago";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
